14e0510ac41ebaff6c8e7876f560cb7a3c8907cb
zhangrong <zr20920@163.com>
2021-08-31 14:38:09 +0800

    '项目模板初始化'

---
+++ src/home/Index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="cnt">
+    <Header></Header>
+    <div>
+      <button @click="onClickJump">当前页跳转</button>
+      <button @click="onClickOpen">新开页面跳转</button>
+    </div>
+    <!-- 样式隐藏 -->
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Header from '../common/Header.vue'
+import Footer from '../common/Footer.vue'
+
+export default Vue.extend({
+  name: 'Home',
+  components: {
+    Header,
+    Footer,
+  },
+  created() {
+    window.addEventListener('wxload', query => console.log('page1 wxload', query))
+    window.addEventListener('wxshow', () => console.log('page1 wxshow'))
+    window.addEventListener('wxready', () => console.log('page1 wxready'))
+    window.addEventListener('wxhide', () => console.log('page1 wxhide'))
+    window.addEventListener('wxunload', () => console.log('page1 wxunload'))
+
+    if (process.env.isMiniprogram) {
+      console.log('I am in miniprogram')
+    } else {
+      console.log('I am in Web')
+    }
+  },
+  methods: {
+    onClickJump() {
+      window.location.href = '/test/list/123'
+    },
+
+    onClickOpen() {
+      window.open('/test/detail/123')
+    },
+  },
+})
+</script>
+
+<style lang="less">
+.cnt {
+  margin-top: 20px;
+}
+
+a, button {
+  display: block;
+  width: 100%;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  font-size: 20px;
+  border: 1px solid #ddd;
+}
+
+.miniprogram-root {
+  .for-web {
+    display: none;
+  }
+}
+</style>
